1f8697b1741ddd3e7484c1af84fcd1b3f5e13cf5
unknown <ardvanbalkom@gmail.com>
2019-04-12 20:43:09 +0100

    added scatterplots

--- Iris_data_comparison_pandas.py
+++ Iris_data_comparison_pandas.py
@@ -10,45 +10,66 @@ ifds = pd.read_excel("iris.xls", usecols=4) #Read the file 'ifds' into the progr
 
 # by entering print(iris), we are now able to print the entire table of data.
 
-print(ifds.describe()) # This displays a quick summary of the data (mean, min and max amongst others).
+print()
+print(round(ifds.describe(),2)) # This displays a quick summary of the data (mean, min and max amongst others). I round it to 2 decimals, because it looks better.
 print() # Print an empty line, to make it look a bit more presentable.
 
+print("Median:")
 print(ifds.median()) # Display the median of all data, separated per column.
 print()
 
+print("Mode:")
+print()
 print(ifds.mode())  # Display the mode of all data.
 print()
 
 setosa = ifds[ifds.iris == "Iris-setosa"] # Only show data for the species "setosa", by only using the rows with "Iris-setosa" in them.
 # Used https://stackoverflow.com/questions/17071871/select-rows-from-a-dataframe-based-on-values-in-a-column-in-pandas as inspiration.
 
-print(setosa.describe()) # Display a quick summary of all data of the "setosa".
+print("Iris setosa:")
+print()
+print(round(setosa.describe(),2)) # Display a quick summary of all data of the "setosa".
 print()
 
+print("Median:")
 print(setosa.median()) # Display the median of all data of the species "setosa". Learned how to use median and mode at: 
 # https://www.geeksforgeeks.org/python-pandas-dataframe-mode/
 print()
 
+print("Mode:")
+print()
 print(setosa.mode()) # Diplay the mode of all data of the species "setosa".
+print()
 
 versicolor = ifds[ifds.iris == "Iris-versicolor"] # Do the same for the species "versicolor"
 
-print(versicolor.describe())
+print("Iris versicolor:")
+print()
+print(round(versicolor.describe(),2))
 print()
 
+print("Median:")
 print(versicolor.median())
 print()
 
+print("Mode:")
+print()
 print(versicolor.mode())
 print()
 
 virginica = ifds[ifds.iris == "Iris-virginica"] # Do the same for the species "virginica"
 
-print(virginica.describe())
+print("iris virginica:")
+print()
+print(round(virginica.describe(),2))
 print()
 
+print("Median:")
 print(virginica.median())
 print()
 
+print("Mode:")
+print()
 print(virginica.mode())
 print()
+
---
+++ Seabornscatterplot.py
@@ -0,0 +1,29 @@
+
+import seaborn as sns # I came across the Seaborn visualization library here: 
+# https://www.datacamp.com/community/tutorials/seaborn-python-tutorial
+# It has the Iris flower data set built in, so that is very handy.
+import matplotlib.pyplot as plt
+
+# Because the dataset is built in, all we have to do is put in the right name: "Iris".
+iris = sns.load_dataset("iris")
+
+sns.scatterplot(x=iris.sepal_length, y=iris.sepal_width, hue=iris.species) # Create a scatterplot that shows the correlation between sepal length and sepal width. 
+# Using the sepal length on the x-axis and the sepal width on the y-axis. By using 'hue', I am able to give the points different colors for every species.
+plt.show()
+
+sns.scatterplot(x=iris.sepal_length, y=iris.petal_length, hue=iris.species) # Do the same for the correlation between sepal length and petal length.
+plt.show()
+
+sns.scatterplot(x=iris.sepal_length, y=iris.petal_width, hue=iris.species) # Do the same for other possible correlations
+plt.show()
+
+sns.scatterplot(x=iris.sepal_width, y=iris.petal_length, hue=iris.species)
+plt.show()
+
+sns.scatterplot(x=iris.sepal_width, y=iris.petal_width, hue=iris.species)
+plt.show()
+
+sns.scatterplot(x=iris.petal_length, y=iris.petal_width, hue=iris.species)
+plt.show()
+
+# Learned about scatterplots here: https://seaborn.pydata.org/tutorial/relational.html#relational-tutorial and how to do a multidimensional one here: https://seaborn.pydata.org/generated/seaborn.scatterplot.html
