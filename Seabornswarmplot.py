b6df4a8b8c61bad64ecabad13062ef3461e1a629
unknown <ardvanbalkom@gmail.com>
2019-04-18 18:23:26 +0100

    added comments

--- Seabornswarmplot.py
+++ Seabornswarmplot.py
@@ -10,7 +10,7 @@ import matplotlib.pyplot as plt
 # Because the dataset is built in, all we have to do is put in the right name: "Iris".
 iris = sns.load_dataset("iris")
 
-# Built the Iris plot, using the swarmplot option:
+# Built the swarmplot:
 sns.swarmplot(x="species", y="petal_width", data=iris) #By changing y to 'petal_length', 'sepal_length', or 'sepal_width', those will be plotted. 
 # Screenshots of the other 3 swarmplots can be found seperately in the repository, as well as in the readme file: https://github.com/Ardvb/Pands-Iris-Project-2019/blob/master/README.md
 # I have plotted them and attached the plots in separate files.
